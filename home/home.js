3eea0ea632f6120da3c9801b4c57d2227093ec17
Socratis <164081143+SocratisGomes@users.noreply.github.com>
2024-06-02 19:27:14 -0300

    atualização

--- home/home.js
+++ home/home.js
@@ -19,3 +19,28 @@ function relogio(){
   }
   relogio()
   setInterval(relogio, 1000)
+
+  /// Cria o gráfico de tarefas por prioridade
+let dadosGrafico = [0, 0, 0];
+let seriesGrafico = {
+  labels: ["Alta", "Média", "Baixa"],
+  datasets: [
+    {
+      label: "Tarefas",
+      data: dadosGrafico,
+      borderWidth: 1,
+    },
+  ],
+};
+let chart = new Chart(grafico, {
+  type: "bar",
+  data: seriesGrafico,
+  options: {
+    y: {
+      beginAtZero: true,
+      ticks: {
+        stepSize: 1,
+      },
+    },
+  },
+});

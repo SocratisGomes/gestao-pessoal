88dc689f72835f925337da63710a36605b8b6d5f
Socratis <164081143+SocratisGomes@users.noreply.github.com>
2024-06-08 23:57:37 -0300

    atualização

--- financeiro/financeiro.js
+++ financeiro/financeiro.js
@@ -43,90 +43,88 @@ function deleteTarefa(idExcluida) {
   tarefas.splice(indice, 1);
   updateTarefas(tarefas);
 }
-
 //Inicia o banco de tarefas existentes no LocalStorage
-function IniciaBanco(){
-  // localStorage.setItem("tarefas", JSON.stringify([]));
-  BancoTarefas = readTarefas()
-  let conteudo = "";
-  BancoTarefas.forEach((item) => {
-    conteudo += `
-      <tr id='linha-${item.id}'>
-        <td class='selecao'>
-          <input type="radio" name="campoSelecao" value="${item.id}" />
-        </td>
-        <td class="celula-1">
-          ${item.descricao}
-        </td>
-        <td class="celula-2"> 
-          ${formataData(item.data)}
-        </td>
-        <td class="celula-3">
-          ${prioridade(item.prioridade)}
-        </td>
-        <td></td>
-      </tr>
-    `;
-  })
-  corpoTabela.innerHTML = conteudo;
-}
+// function IniciaBanco(){
+//   // localStorage.setItem("tarefas", JSON.stringify([]));
+//   BancoTarefas = readTarefas()
+//   let conteudo = "";
+//   BancoTarefas.forEach((item) => {
+//     conteudo += `
+//       <tr id='linha-${item.id}'>
+//         <td class='selecao'>
+//           <input type="radio" name="campoSelecao" value="${item.id}" />
+//         </td>
+//         <td class="celula-1">
+//           ${item.descricao}
+//         </td>
+//         <td class="celula-2"> 
+//           ${formataData(item.data)}
+//         </td>
+//         <td class="celula-3">
+//           ${prioridade(item.prioridade)}
+//         </td>
+//         <td></td>
+//       </tr>
+//     `;
+//   })
+//   corpoTabela.innerHTML = conteudo;
+// }
 
 
-// Desenha a tabela com novas tarefas 
-function mostraTabela() {
-    // carrega dados
-  let listaDeTarefas = readTarefas();
-  dados = listaDeTarefas.filter(
-    (t) =>
-      filtro.value === "" ||
-      t.descricao.toLowerCase().includes(filtro.value.toLowerCase())
-  );
+// // Desenha a tabela com novas tarefas 
+// function mostraTabela() {
+//     // carrega dados
+//   let listaDeTarefas = readTarefas();
+//   dados = listaDeTarefas.filter(
+//     (t) =>
+//       filtro.value === "" ||
+//       t.descricao.toLowerCase().includes(filtro.value.toLowerCase())
+//   );
 
-  // gera conteúdo da tabela
-  let conteudo = "";
-  dados.forEach((item) => {
-    conteudo += `
-      <tr id='linha-${item.id}'>
-        <td class='selecao'>
-          <input type="radio" name="campoSelecao" value="${item.id}" />
-        </td>
-        <td class="celula-1">
-          ${item.descricao}
-        </td>
-        <td class="celula-2"> 
-          ${formataData(item.data)}
-        </td>
-        <td class="celula-3">
-          ${prioridade(item.prioridade)}
-        </td>
-        <td></td>
-      </tr>
-    `;
-  });
-  corpoTabela.innerHTML = conteudo;
+//   // gera conteúdo da tabela
+//   let conteudo = "";
+//   dados.forEach((item) => {
+//     conteudo += `
+//       <tr id='linha-${item.id}'>
+//         <td class='selecao'>
+//           <input type="radio" name="campoSelecao" value="${item.id}" />
+//         </td>
+//         <td class="celula-1">
+//           ${item.descricao}
+//         </td>
+//         <td class="celula-2"> 
+//           ${formataData(item.data)}
+//         </td>
+//         <td class="celula-3">
+//           ${prioridade(item.prioridade)}
+//         </td>
+//         <td></td>
+//       </tr>
+//     `;
+//   });
+//   corpoTabela.innerHTML = conteudo;
 
-  // determina comportamento dos botões e outros componentes interativos
-  let botoesSelecao = document.querySelectorAll("input[name=campoSelecao]");
-  botoesSelecao.forEach((b) => {
-    b.onclick = function (e) {
-      btEditar.disabled = false;
-      btExcluir.disabled = false;
-    };
-  });
-  btAdicionar.disabled = false;
-  btEditar.disabled = true;
-  btExcluir.disabled = true;
+//   // determina comportamento dos botões e outros componentes interativos
+//   let botoesSelecao = document.querySelectorAll("input[name=campoSelecao]");
+//   botoesSelecao.forEach((b) => {
+//     b.onclick = function (e) {
+//       btEditar.disabled = false;
+//       btExcluir.disabled = false;
+//     };
+//   });
+//   btAdicionar.disabled = false;
+//   btEditar.disabled = true;
+//   btExcluir.disabled = true;
 
-  // atualiza gráfico de tarefas por prioridade
-  // dadosGrafico = [0, 0, 0];
-  // dados.forEach((t) => dadosGrafico[t.prioridade - 1]++);
-  // chart.data.datasets[0].data = dadosGrafico;
-  // chart.update();
-}
+//   // atualiza gráfico de tarefas por prioridade
+//   // dadosGrafico = [0, 0, 0];
+//   // dados.forEach((t) => dadosGrafico[t.prioridade - 1]++);
+//   // chart.data.datasets[0].data = dadosGrafico;
+//   // chart.update();
+// }
 
 // Mostra a janela modal para criação de nova tarefa
-btAdicionar.onclick = function () {
-  modalTarefa.style.display = "block";
+btAdicionar.onclick = function () {  
   campoMes.value = "";
   campoSal.value = "";
   campoExtra.value = "";
@@ -137,8 +135,7 @@ btAdicionar.onclick = function () {
   campoDinheiro.value = "";
   campoSobra.value = "";
   campoSal.disabled = false;
-  // campoPrioridade.disabled = false;
-  
+  modalTarefa.style.display = "block";
   btMTCriar.style.display = "inline-block";
   btMTAlterar.style.display = "none";
   btMTExcluir.style.display = "none";
@@ -148,6 +145,7 @@ btAdicionar.onclick = function () {
 
 // Verifica se os três campos estão preenchidos antes de criar ou alterar tarefa
 let liberaBotaoMT = function () {
+  let bloqueiaInput = document.querySelector('#campoSobra')
   if (
     campoMes.value.length > 0 &&
     campoSal.value.length > 0 &&
@@ -156,11 +154,12 @@ let liberaBotaoMT = function () {
     campoInter.value.length > 0 &&
     campoC6.value.length > 0 &&
     campoInvest.value.length > 0 &&
-    campoDinheiro.value.length > 0 &&
-    campoSobra.value.length > 0
+    campoDinheiro.value.length > 0 
   ) {
     btMTCriar.disabled = false;
     btMTAlterar.disabled = false;
+    bloqueiaInput.disabled = true;
+    calculoSobra()    
   } else {
     btMTCriar.disabled = true;
     btMTAlterar.disabled = true;
@@ -175,141 +174,155 @@ campoC6.onkeyup = liberaBotaoMT;
 campoInvest.onkeyup = liberaBotaoMT;
 campoDinheiro.onkeyup = liberaBotaoMT;
 campoSobra.onkeyup = liberaBotaoMT;
-// campoPrioridade.onchange = liberaBotaoMT;
+
+//Calcula a sobra das receitas - despesas e é add no campo sobra na mesma tela após o onkeyup
+function calculoSobra(){
+  let salario = parseFloat(document.getElementById('campoSal').value);
+  let extra = parseFloat(document.getElementById('campoExtra').value);
+  let nubank = parseFloat(document.getElementById('campoNu').value);
+  let inter = parseFloat(document.getElementById('campoInter').value);
+  let c6 = parseFloat(document.getElementById('campoC6').value);
+  let invest = parseFloat(document.getElementById('campoInvest').value);
+  let dinheiro = parseFloat(document.getElementById('campoDinheiro').value)
+   
+  let result = ((salario + extra) - (nubank + inter + c6 + invest + dinheiro))
+
+  document.getElementById('campoSobra').value = result  
+}
 
 // Confirma a criação da tarefa
-btMTCriar.onclick = function () {
-  let tarefa = {
-    descricao: campoMes.value,
-    data: campoSal.value,
-    // prioridade: campoPrioridade.value,
-  };
-  createTarefa(tarefa, mostraTabela);
-  modalTarefa.style.display = "none";
-};
+// btMTCriar.onclick = function () {
+//   let tarefa = {
+//     descricao: campoMes.value,
+//     data: campoSal.value,
+//     // prioridade: campoPrioridade.value,
+//   };
+//   createTarefa(tarefa, mostraTabela);
+//   modalTarefa.style.display = "none";
+// };
 
-// Mostra a janela modal para edição de uma tarefa existente
-btEditar.onclick = function () {
-  let selecoes = Array.from(
-    document.querySelectorAll("input[name=campoSelecao]")
-  );
-  let selecionado = selecoes.find((i) => i.checked == true);
-  if (selecionado) {
-    let tarefa = readTarefa(selecionado.value);
-    campoMes.value = tarefa.descricao;
-    campoSal.value = tarefa.data;
-    campoPrioridade.value = tarefa.prioridade;
-    campoID.value = tarefa.id;
-    campoMes.disabled = false;
-    campoSal.disabled = false;
-    campoPrioridade.disabled = false;
-    modalTarefa.style.display = "block";
-    btMTCriar.style.display = "none";
-    btMTAlterar.style.display = "inline-block";
-    btMTExcluir.style.display = "none";
-    btMTAlterar.disabled = false;
-    campoMes.focus();
-  }
-};
+// // Mostra a janela modal para edição de uma tarefa existente
+// btEditar.onclick = function () {
+//   let selecoes = Array.from(
+//     document.querySelectorAll("input[name=campoSelecao]")
+//   );
+//   let selecionado = selecoes.find((i) => i.checked == true);
+//   if (selecionado) {
+//     let tarefa = readTarefa(selecionado.value);
+//     campoMes.value = tarefa.descricao;
+//     campoSal.value = tarefa.data;
+//     campoPrioridade.value = tarefa.prioridade;
+//     campoID.value = tarefa.id;
+//     campoMes.disabled = false;
+//     campoSal.disabled = false;
+//     campoPrioridade.disabled = false;
+//     modalTarefa.style.display = "block";
+//     btMTCriar.style.display = "none";
+//     btMTAlterar.style.display = "inline-block";
+//     btMTExcluir.style.display = "none";
+//     btMTAlterar.disabled = false;
+//     campoMes.focus();
+//   }
+// };
 
-// Confirma a alteração da tarefa
-btMTAlterar.onclick = function () {
-  let tarefaEditada = {
-    id: campoID.value,
-    descricao: campoMes.value,
-    data: campoSal.value,
-    prioridade: campoPrioridade.value,
-  };
-  updateTarefa(tarefaEditada);
-  modalTarefa.style.display = "none";
-  mostraTabela();
-};
+// // Confirma a alteração da tarefa
+// btMTAlterar.onclick = function () {
+//   let tarefaEditada = {
+//     id: campoID.value,
+//     descricao: campoMes.value,
+//     data: campoSal.value,
+//     prioridade: campoPrioridade.value,
+//   };
+//   updateTarefa(tarefaEditada);
+//   modalTarefa.style.display = "none";
+//   mostraTabela();
+// };
 
-// Mostra a janela modal para exclusão de uma tarefa existente
-btExcluir.onclick = function () {
-  let selecoes = Array.from(
-    document.querySelectorAll("input[name=campoSelecao]")
-  );
-  let selecionado = selecoes.find((i) => i.checked == true);
-  if (selecionado) {
-    let tarefa = readTarefa(selecionado.value);
-    campoMes.value = tarefa.descricao;
-    campoSal.value = tarefa.data;
-    campoPrioridade.value = tarefa.prioridade;
-    campoID.value = tarefa.id;
-    campoMes.disabled = true;
-    campoSal.disabled = true;
-    campoPrioridade.disabled = true;
-    modalTarefa.style.display = "block";
-    btMTCriar.style.display = "none";
-    btMTAlterar.style.display = "none";
-    btMTExcluir.style.display = "inline-block";
-  }
-};
+// // Mostra a janela modal para exclusão de uma tarefa existente
+// btExcluir.onclick = function () {
+//   let selecoes = Array.from(
+//     document.querySelectorAll("input[name=campoSelecao]")
+//   );
+//   let selecionado = selecoes.find((i) => i.checked == true);
+//   if (selecionado) {
+//     let tarefa = readTarefa(selecionado.value);
+//     campoMes.value = tarefa.descricao;
+//     campoSal.value = tarefa.data;
+//     campoPrioridade.value = tarefa.prioridade;
+//     campoID.value = tarefa.id;
+//     campoMes.disabled = true;
+//     campoSal.disabled = true;
+//     campoPrioridade.disabled = true;
+//     modalTarefa.style.display = "block";
+//     btMTCriar.style.display = "none";
+//     btMTAlterar.style.display = "none";
+//     btMTExcluir.style.display = "inline-block";
+//   }
+// };
 
-// Confirma a exclusão da tarefa
-btMTExcluir.onclick = function () {
-  deleteTarefa(campoID.value);
-  modalTarefa.style.display = "none";
-  mostraTabela();
-};
+// // Confirma a exclusão da tarefa
+// btMTExcluir.onclick = function () {
+//   deleteTarefa(campoID.value);
+//   modalTarefa.style.display = "none";
+//   mostraTabela();
+// };
 
-// Cancela a criação, alteração ou exclusão da tarefa
-btMTCancelar.onclick = function () {
-  modalTarefa.style.display = "none";
-};
+// // Cancela a criação, alteração ou exclusão da tarefa
+// btMTCancelar.onclick = function () {
+//   modalTarefa.style.display = "none";
+// };
 
 // Configura o botão de fechar a janela modal
 fechaModal.onclick = function () {
   modalTarefa.style.display = "none";
 };
 
-// Ordenação pelas três colunas
-th1.onclick = () => {
-  let dados = readTarefas();
-  dados.sort((a, b) =>
-    a.descricao.localeCompare(b.descricao, "pt-br", { sensitivity: "base" })
-  );
-  updateTarefas(dados);
-  mostraTabela();
-};
-th2.onclick = () => {
-  let dados = readTarefas();
-  dados.sort((a, b) =>
-    a.data.localeCompare(b.data, "pt-br", { sensitivity: "base" })
-  );
-  updateTarefas(dados);
-  mostraTabela();
-};
-th3.onclick = () => {
-  let dados = readTarefas();
-  dados.sort((a, b) =>
-    a.prioridade.localeCompare(b.prioridade, "pt-br", { sensitivity: "base" })
-  );
-  updateTarefas(dados);
-  mostraTabela();
-};
+// // Ordenação pelas três colunas
+// th1.onclick = () => {
+//   let dados = readTarefas();
+//   dados.sort((a, b) =>
+//     a.descricao.localeCompare(b.descricao, "pt-br", { sensitivity: "base" })
+//   );
+//   updateTarefas(dados);
+//   mostraTabela();
+// };
+// th2.onclick = () => {
+//   let dados = readTarefas();
+//   dados.sort((a, b) =>
+//     a.data.localeCompare(b.data, "pt-br", { sensitivity: "base" })
+//   );
+//   updateTarefas(dados);
+//   mostraTabela();
+// };
+// th3.onclick = () => {
+//   let dados = readTarefas();
+//   dados.sort((a, b) =>
+//     a.prioridade.localeCompare(b.prioridade, "pt-br", { sensitivity: "base" })
+//   );
+//   updateTarefas(dados);
+//   mostraTabela();
+// };
 
-// funções de apoio para formatar os valores da tabela
-function prioridade(p) {
-  let prioridades = ["Alta", "Média", "Baixa"];
-  return prioridades[parseInt(p) - 1];
-}
-function formataData(d) {
-  [ano, mes, dia] = d.split("-");
-  return dia + "-" + mes + "-" + ano;
-}
+// // funções de apoio para formatar os valores da tabela
+// function prioridade(p) {
+//   let prioridades = ["Alta", "Média", "Baixa"];
+//   return prioridades[parseInt(p) - 1];
+// }
+// function formataData(d) {
+//   [ano, mes, dia] = d.split("-");
+//   return dia + "-" + mes + "-" + ano;
+// }
 
-// Funções para tratamento do filtro
-limpaFiltro.onclick = function () {
-  filtro.value = "";
-  mostraTabela();
-};
+// // Funções para tratamento do filtro
+// limpaFiltro.onclick = function () {
+//   filtro.value = "";
+//   mostraTabela();
+// };
 
-filtro.onkeyup = function () {
-  mostraTabela();
-};
+// filtro.onkeyup = function () {
+//   mostraTabela();
+// };
 
-// Após preparar todo o código, desenha a versão preliminar da tabela, com dados já existentes
-IniciaBanco();
-mostraTabela();
+// // Após preparar todo o código, desenha a versão preliminar da tabela, com dados já existentes
+// IniciaBanco();
+// mostraTabela();
